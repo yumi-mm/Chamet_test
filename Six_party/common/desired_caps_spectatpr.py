1f1e4215eae822acdaf316e4b213fa72c87f08a8
yumi-mm <849517666@qq.com>
2023-07-07 14:39:38 +0800

    SixParty Commit

---
+++ Six_party/common/desired_caps_spectatpr.py
@@ -0,0 +1,52 @@
+from appium import webdriver
+import yaml
+import logging
+import logging.config
+import os
+from appium.webdriver.common.multi_action import MultiAction
+from appium.webdriver.common.touch_action import TouchAction
+from appium import webdriver
+
+CON_LOG='../config/log_conf.py'
+logging.config.fileConfig(CON_LOG)
+logging = logging.getLogger()
+
+
+def spectator_appium_desired():
+    with open('../config/caps_110phone.yaml.py', 'r', encoding='utf-8') as file:
+        data=yaml.load(file, Loader=yaml.FullLoader)
+
+    print(data)
+    desired_caps = {}
+    # desired_caps['platformVersion'] = data['platformVersion']
+    desired_caps['deviceName'] = data['deviceName']
+    desired_caps['platformName'] = data['platformName']
+    # 可用于调用apk包的路径
+    # base_dir = os.path.dirname(os.path.dirname(__file__))
+    # app_path = os.path.join(base_dir, 'app', data['appPackage'])
+    desired_caps['appPackage'] = data['appPackage']
+    desired_caps['appActivity'] = data['appActivity']
+    # desired_caps['automationName'] = data['automationName']
+    desired_caps['noReset'] = data['noReset']
+
+    desired_caps['unicodeKeyboard'] = data['unicodeKeyboard']
+    desired_caps['resetKeyboard'] = data['resetKeyboard']
+    desired_caps['udid'] = data['udid']
+    desired_caps['newCommandTimeout'] = data['newCommandTimeout']
+    logging.info('start app......')
+    driver = webdriver.Remote('http://localhost' + ":" + str(data['port']) + '/wd/hub', desired_caps)
+    driver.implicitly_wait(8)
+    return driver
+
+
+if __name__== '__main__':
+    spectator_appium_desired()
+
+#     验证
+#     with open('../config/caps_110phone.yaml.py', 'r', encoding='utf-8') as file:
+#         data = yaml.load(file, Loader=yaml.FullLoader)
+#     base_dir = os.path.dirname(os.path.dirname(__file__))
+#     print(os.path.dirname(__file__))
+#     print(base_dir)
+#     app_path = os.path.join(base_dir,'app',data['appPackage'])
+#     print(app_path)
